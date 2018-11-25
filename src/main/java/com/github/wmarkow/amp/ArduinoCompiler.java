a728c03b4023bd56cc79212cf63bb4be35603461
Witold Markowski <witold.a.markowski@gmail.com>
2018-11-25 21:30:59 +0000

    Refactor compilers.

---
+++ src/main/java/com/github/wmarkow/amp/ArduinoCompiler.java
@@ -0,0 +1,40 @@
+package com.github.wmarkow.amp;
+
+import java.util.Vector;
+
+import com.github.maven_nar.cpptasks.OptimizationEnum;
+import com.github.maven_nar.cpptasks.compiler.LinkType;
+import com.github.maven_nar.cpptasks.compiler.Linker;
+import com.github.maven_nar.cpptasks.gcc.GccCompatibleCCompiler;
+
+public abstract class ArduinoCompiler extends GccCompatibleCCompiler
+{
+
+    private final static String[] headerExtensions = new String[]
+    { ".h" };
+
+    protected ArduinoCompiler( String command, String[] sourceExtensions )
+    {
+        super( command, null, sourceExtensions, headerExtensions, false, null, false, null );
+    }
+
+    @Override
+    public Linker getLinker( final LinkType linkType )
+    {
+        return new ArduinoLinker().getLinker( linkType );
+    }
+
+    @Override
+    public int getMaximumCommandLength()
+    {
+        return isWindows() ? 20000 : Integer.MAX_VALUE;
+    }
+
+    @Override
+    public void addImpliedArgs( final Vector< String > args, final boolean debug,
+        final boolean multithreaded, final boolean exceptions, final LinkType linkType, final Boolean rtti,
+        final OptimizationEnum optimization )
+    {
+        // do nothing
+    }
+}
--- src/main/java/com/github/wmarkow/amp/ArduinoCppCompiler.java
+++ src/main/java/com/github/wmarkow/amp/ArduinoCppCompiler.java
@@ -1,53 +1,12 @@
 package com.github.wmarkow.amp;
 
-import java.util.Vector;
-
-import org.apache.tools.ant.types.Environment;
-
-import com.github.maven_nar.cpptasks.OptimizationEnum;
-import com.github.maven_nar.cpptasks.compiler.LinkType;
-import com.github.maven_nar.cpptasks.compiler.Linker;
-import com.github.maven_nar.cpptasks.gcc.GccCompatibleCCompiler;
-
-public class ArduinoCppCompiler extends GccCompatibleCCompiler
+public class ArduinoCppCompiler extends ArduinoCompiler
 {
     private final static String[] sourceExtensions = new String[]
     { ".cpp", };
 
-    private final static String[] headerExtensions = new String[]
-    { ".h" };
-
     public ArduinoCppCompiler()
     {
-        this( "g++", sourceExtensions, headerExtensions, false, new ArduinoCppCompiler( "g++",
-            sourceExtensions, headerExtensions, true, null, false, null ), false, null );
-    }
-
-    private ArduinoCppCompiler( final String command, final String[] sourceExtensions,
-        final String[] headerExtensions, final boolean isLibtool, final ArduinoCppCompiler libtoolCompiler,
-        final boolean newEnvironment, final Environment env )
-    {
-        super( command, null, sourceExtensions, headerExtensions, isLibtool, libtoolCompiler, newEnvironment,
-            env );
-    }
-
-    @Override
-    public Linker getLinker( final LinkType linkType )
-    {
-        return new ArduinoLinker().getLinker( linkType );
-    }
-
-    @Override
-    public int getMaximumCommandLength()
-    {
-        return isWindows() ? 20000 : Integer.MAX_VALUE;
-    }
-
-    @Override
-    public void addImpliedArgs( final Vector< String > args, final boolean debug,
-        final boolean multithreaded, final boolean exceptions, final LinkType linkType, final Boolean rtti,
-        final OptimizationEnum optimization )
-    {
-        // do nothing
+        super( "g++", sourceExtensions );
     }
 }
