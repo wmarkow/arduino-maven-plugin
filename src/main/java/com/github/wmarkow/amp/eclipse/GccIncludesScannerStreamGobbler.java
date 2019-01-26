714b7a4261e921b5d9d7fb085179465122c7100e
Witold Markowski <witold.a.markowski@gmail.com>
2019-01-26 21:45:33 +0000

    Parse default GCC includes.

--- src/main/java/com/github/wmarkow/amp/eclipse/GccIncludesScannerStreamGobbler.java
+++ src/main/java/com/github/wmarkow/amp/eclipse/GccIncludesScannerStreamGobbler.java
@@ -1,5 +1,9 @@
 package com.github.wmarkow.amp.eclipse;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +13,57 @@ public class GccIncludesScannerStreamGobbler extends StreamGobbler
 {
     private Logger logger = LoggerFactory.getLogger( GccIncludesScannerStreamGobbler.class );
 
+    private boolean includesStartsHereDetected = false;
+    private boolean includesFound = false;
+
+    private ArrayList< File > defaultIncludes = new ArrayList< File >();
+
+    public File[] getDefaultIncludes()
+    {
+        return defaultIncludes.toArray( new File[]
+        {} );
+    }
+
     @Override
     protected void onLineRead( String line )
     {
-        logger.info( String.format( "          %s", line ) );
+        if( includesFound )
+        {
+            return;
+        }
+
+        if( line.toLowerCase().matches( ".*include.+search starts here.*" ) )
+        {
+            includesStartsHereDetected = true;
+
+            return;
+        }
+
+        if( includesStartsHereDetected && line.toLowerCase().matches( ".*end of search list.*" ) )
+        {
+            includesFound = true;
+
+            return;
+        }
+
+        if( includesStartsHereDetected && includesFound == false )
+        {
+            try
+            {
+                File file = parseIntoFile( line );
+                defaultIncludes.add( file );
+                logger.info( String.format( " Found GCC inlude path as %s", file.getAbsolutePath() ) );
+            }
+            catch( IOException e )
+            {
+                logger.warn( "Exception when parsing GCC include path %s line", e );
+            }
+        }
+    }
+
+    private File parseIntoFile( String filePath ) throws IOException
+    {
+        File file = new File( filePath.trim() );
+        return file.getCanonicalFile();
     }
 }
