c006ef32b933568e9afdb20f2247fb36df3e2cdc
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-09 21:26:59 +0000

    Fix some unit and integration tests for amp-arduino.

--- amp-arduino/src/integration-test/java/com/github/wmarkow/amp/arduino/platform/manager/PlatformToolsManagerIntegrationTest.java
+++ amp-arduino/src/integration-test/java/com/github/wmarkow/amp/arduino/platform/manager/PlatformToolsManagerIntegrationTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 
-import org.codehaus.plexus.util.FileUtils;
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 
