0c5e5dbb61a3e9c4aa05c2ae8b6a71bc34c4d39b
Witold Markowski <witold.a.markowski@gmail.com>
2020-01-10 19:42:07 +0000

    Fix tests in amp-arduino.

--- amp-arduino/src/main/java/com/github/wmarkow/amp/arduino/platform/PlatformRepository.java
+++ amp-arduino/src/main/java/com/github/wmarkow/amp/arduino/platform/PlatformRepository.java
@@ -3,38 +3,28 @@ package com.github.wmarkow.amp.arduino.platform;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.github.wmarkow.amp.util.ArtifactUtils;
+public class PlatformRepository {
+    private List<PlatformPackageIndex> indexes = new ArrayList<PlatformPackageIndex>();
 
-public class PlatformRepository
-{
-    private List< PlatformPackageIndex > indexes = new ArrayList< PlatformPackageIndex >();
-
-    public void addIndex( PlatformPackageIndex index )
-    {
-        indexes.add( index );
+    public void addIndex(PlatformPackageIndex index) {
+        indexes.add(index);
     }
 
-    public List< Package > getPackages()
-    {
-        List< Package > result = new ArrayList< Package >();
+    public List<Package> getPackages() {
+        List<Package> result = new ArrayList<Package>();
 
-        for( PlatformPackageIndex index : indexes )
-        {
-            result.addAll( index.getPackages() );
+        for (PlatformPackageIndex index : indexes) {
+            result.addAll(index.getPackages());
         }
 
         return result;
     }
 
-    public Package getPackage( String artifactId, String version )
-    {
-        for( Package _package : getPackages() )
-        {
-            for( Platform platform : _package.getPlatforms() )
-            {
+    public Package getPackage(String artifactId, String version) {
+        for (Package _package : getPackages()) {
+            for (Platform platform : _package.getPlatforms()) {
                 final String _artifactId = _package.getName() + "-" + platform.getArchitecture();
-                if( artifactId.equals( _artifactId ) && version.equals( platform.getVersion() ) )
-                {
+                if (artifactId.equals(_artifactId) && version.equals(platform.getVersion())) {
                     return _package;
                 }
             }
@@ -43,12 +33,9 @@ public class PlatformRepository
         return null;
     }
 
-    public Package getPackageByName( String name )
-    {
-        for( Package _package : getPackages() )
-        {
-            if( _package.getName().equals( name ) )
-            {
+    public Package getPackageByName(String name) {
+        for (Package _package : getPackages()) {
+            if (_package.getName().equals(name)) {
                 return _package;
             }
         }
@@ -56,15 +43,11 @@ public class PlatformRepository
         return null;
     }
 
-    public Platform getPlatform( String artifactId, String version )
-    {
-        for( Package _package : getPackages() )
-        {
-            for( Platform platform : _package.getPlatforms() )
-            {
-                final String _artifactId = ArtifactUtils.toArtifactId( _package, platform );
-                if( artifactId.equals( _artifactId ) && version.equals( platform.getVersion() ) )
-                {
+    public Platform getPlatform(String packageName, String platformArchitecture, String version) {
+        for (Package _package : getPackages()) {
+            for (Platform platform : _package.getPlatforms()) {
+                if (_package.getName().equals(packageName) && platform.getArchitecture().equals(platformArchitecture)
+                        && version.equals(platform.getVersion())) {
                     return platform;
                 }
             }
@@ -73,16 +56,11 @@ public class PlatformRepository
         return null;
     }
 
-    public Tool getToolByPackagerAndNameAndVersion( String packager, String name, String version )
-    {
-        for( Package _package : getPackages() )
-        {
-            if( _package.getName().equals( packager ) )
-            {
-                for( Tool tool : _package.getTools() )
-                {
-                    if( tool.getName().equals( name ) && tool.getVersion().equals( version ) )
-                    {
+    public Tool getToolByPackagerAndNameAndVersion(String packager, String name, String version) {
+        for (Package _package : getPackages()) {
+            if (_package.getName().equals(packager)) {
+                for (Tool tool : _package.getTools()) {
+                    if (tool.getName().equals(name) && tool.getVersion().equals(version)) {
                         return tool;
                     }
                 }
--- amp-arduino/src/test/java/com/github/wmarkow/amp/arduino/platform/PlatformRepositoryTest.java
+++ amp-arduino/src/test/java/com/github/wmarkow/amp/arduino/platform/PlatformRepositoryTest.java
@@ -10,52 +10,41 @@ import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 
-public class PlatformRepositoryTest
-{
+public class PlatformRepositoryTest {
 
     private PlatformRepository platformRepository;
 
     @Before
-    public void test() throws IOException
-    {
+    public void test() throws IOException {
         platformRepository = new PlatformRepository();
 
         PlatformFilesReader reader = new PlatformFilesReader();
 
-        platformRepository.addIndex( reader.readFromJson( new File(
-            "src/test/resources/package_digistump_index.json" ) ) );
+        platformRepository.addIndex(reader.readFromJson(new File("src/test/resources/package_digistump_index.json")));
     }
 
     @Test
-    public void testGetPlatformForDigistump() throws IOException
-    {
-        assertNotNull( platformRepository.getPlatform( "digistump-avr", "1.6.7" ) );
+    public void testGetPlatformForDigistump() throws IOException {
+        assertNotNull(platformRepository.getPlatform("digistump", "avr", "1.6.7"));
 
-        assertEquals( 2, platformRepository.getPlatform( "digistump-avr", "1.6.7" ).getToolsDependencies()
-            .size() );
+        assertEquals(2, platformRepository.getPlatform("digistump", "avr", "1.6.7").getToolsDependencies().size());
 
-        ToolsDependency td =
-            platformRepository.getPlatform( "digistump-avr", "1.6.7" ).getToolsDependencies().get( 0 );
-        assertEquals( "arduino", td.getPackager() );
-        assertEquals( "avr-gcc", td.getName() );
-        assertEquals( "4.8.1-arduino5", td.getVersion() );
+        ToolsDependency td = platformRepository.getPlatform("digistump", "avr", "1.6.7").getToolsDependencies().get(0);
+        assertEquals("arduino", td.getPackager());
+        assertEquals("avr-gcc", td.getName());
+        assertEquals("4.8.1-arduino5", td.getVersion());
     }
 
     @Test
-    public void testGetToolByPackagerAndNameAndVersionWhenMissingArduinoIndex()
-    {
-        assertNull( platformRepository.getToolByPackagerAndNameAndVersion( "arduino", "avr-gcc",
-            "4.8.1-arduino5" ) );
+    public void testGetToolByPackagerAndNameAndVersionWhenMissingArduinoIndex() {
+        assertNull(platformRepository.getToolByPackagerAndNameAndVersion("arduino", "avr-gcc", "4.8.1-arduino5"));
     }
 
     @Test
-    public void testGetToolByPackagerAndNameAndVersionWhenArduinoIndex() throws IOException
-    {
+    public void testGetToolByPackagerAndNameAndVersionWhenArduinoIndex() throws IOException {
         PlatformFilesReader reader = new PlatformFilesReader();
-        platformRepository.addIndex( reader
-            .readFromJson( new File( "src/test/resources/package_index.json" ) ) );
+        platformRepository.addIndex(reader.readFromJson(new File("src/test/resources/package_index.json")));
 
-        assertNotNull( platformRepository.getToolByPackagerAndNameAndVersion( "arduino", "avr-gcc",
-            "4.8.1-arduino5" ) );
+        assertNotNull(platformRepository.getToolByPackagerAndNameAndVersion("arduino", "avr-gcc", "4.8.1-arduino5"));
     }
 }
